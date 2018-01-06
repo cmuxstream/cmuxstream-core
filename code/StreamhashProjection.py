7bf400bc82a5b7c4986ffb13d12f0b89396cf25e
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2018-01-06 02:58:44 -0500

    streamhash projections

---
+++ code/StreamhashProjection.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import math
+import numpy as np
+import random
+import mmh3
+
+class StreamhashProjection:
+
+    def __init__(self, n_components, density=0.25, random_state=None):
+        if abs(math.log(density, 2) - int(math.log(density, 2))) > 10**-6:
+            raise Exception("Density must be a negative power of 2.0")
+
+        masksize = int(-math.log(density, 2) + 1)
+        self.mask = 2**masksize - 1
+        self.keys = np.arange(0, n_components, 1)
+        self.constant = np.sqrt(1./density)/np.sqrt(n_components)
+        self.density = density
+        self.n_components = n_components
+        random.seed(random_state)
+
+    def fit_transform(self, X, feature_names=None):
+        nsamples = X.shape[0]
+        ndim = X.shape[1]
+        if feature_names is None:
+            feature_names = [str(i) for i in range(ndim)]
+
+        R = np.array([[self._hash_string(k, f)
+                       for f in feature_names]
+                       for k in self.keys])
+
+        # check if density matches
+        #print "R", np.sum(R.ravel() == 0)/float(len(R.ravel())) 
+
+        Y = np.dot(X, R.T)
+        return Y
+
+    def transform(self, X, feature_names=None):
+        return self.fit_transform(self, X, feature_names)
+    
+    def _hash_string(self, k, s):
+        hash_value = mmh3.hash(s, signed=False, seed=k)
+        last_b_bits = hash_value & self.mask 
+        if last_b_bits == 0:
+            return -1 * self.constant
+        elif last_b_bits == self.mask:
+            return self.constant
+        else:
+            return 0.0
--- code/test_projections.py
+++ code/test_projections.py
@@ -7,65 +7,69 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import loadmat
 from scipy.spatial.distance import pdist
-from sklearn.ensemble import IsolationForest
+from scipy.sparse import csr_matrix
 from sklearn.random_projection import SparseRandomProjection
-from sklearn.metrics import precision_recall_curve, average_precision_score
+from StreamhashProjection import StreamhashProjection
+import sys
+import time
 
 if __name__ == "__main__":
-    data = loadmat("../data/synData.mat")
+    metric = sys.argv[1]
+
+    data = loadmat("../data/synDataNoisy.mat")
     X = data["X"]
     y = data["y"].ravel()
+    Xnoisy = X
 
-    Xnoisy = np.concatenate((X, np.random.normal(loc=0.5, scale=0.05,
-                                                 size=(X.shape[0], 100))), axis=1)
-
-    plt.figure(0)
-    orig_dists = pdist(Xnoisy, metric='euclidean').ravel()
-    print max(orig_dists)
+    orig_dists = pdist(Xnoisy, metric=metric).ravel()
     pd = []
-    for i, k in enumerate(range(50,90,10)):
-        print "k:", k
+    shd = []
+    ks = [50, 100, 1000]
+    for i, k in enumerate(ks):
+        print "k:", k, metric
+        print "SparseRandomProjection...",
+        t0 = time.time()
         projector = SparseRandomProjection(n_components=k,
-                                           density=1/3.0,
+                                           density=1/4.0,
                                            random_state=SEED)
         projected_X = projector.fit_transform(Xnoisy)
+        print "done in", time.time() - t0, "s"
 
-        projected_dists = pdist(projected_X, metric='euclidean')
-        pd.append(projected_dists)
-
-	iforest_clf = IsolationForest(n_estimators=100,
-                                      max_samples=1.0,
-                                      contamination=0.03,
-                                      max_features=1.0,
-                                      bootstrap=False,
-                                      n_jobs=-1,
-                                      random_state=SEED,
-                                      verbose=0)
-        iforest_clf.fit(projected_X)
-        yscore = 0.5 - iforest_clf.decision_function(projected_X)
-        precision, recall, _ = precision_recall_curve(y, yscore, pos_label=1)
-        average_precision = average_precision_score(y, yscore)
-
-        plt.plot(recall, precision, lw=1,
-                 label='k=' + str(k) + ' ap= ' + '{:.3f}'.format(average_precision))
-    
-    plt.xlabel("Recall")
-    plt.ylabel("Precision")
-    plt.grid()
-
-    plt.legend()
-    plt.savefig("pr_curve_vs_k.png", bbox_inches="tight")
-
-    for i, k in enumerate(range(50,90,10)):
-        print "k:", k
-        projected_dists = pd[i].ravel()
+        projected_dists = pdist(projected_X, metric=metric).ravel()
         nonzero = orig_dists != 0
 
-        plt.clf()
+        f, ax = plt.subplots()
         plt.hexbin(orig_dists[nonzero], projected_dists[nonzero],
                    gridsize=100, cmap=plt.cm.Blues, bins='log')
-        plt.xlabel("Original distances (Euclidean)")
+        plt.xlabel("Original distances")
         plt.ylabel("Projected distances")
         cb = plt.colorbar()
-        cb.set_label("Pair count")
-        plt.savefig("dist_histogram_k" + str(k) + ".png", bbox_inches="tight")
+        cb.set_label("log10(count)")
+        plt.grid()
+        ax.set_aspect("equal")
+        plt.savefig("sparse_projection_" + metric + "_k"
+                    + str(k) + ".pdf", bbox_inches="tight")
+
+        print "Streamhash...", metric,
+        t0 = time.time()
+
+        projector = StreamhashProjection(n_components=k,
+                                         density=1/4.0,
+                                         random_state=SEED)
+
+        projected_X = projector.fit_transform(Xnoisy)
+        print "done in", time.time() - t0, "s"
+
+        projected_dists = pdist(projected_X, metric=metric).ravel()
+        nonzero = orig_dists != 0
+
+        f, ax = plt.subplots()
+        plt.hexbin(orig_dists[nonzero], projected_dists[nonzero],
+                   gridsize=100, cmap=plt.cm.Blues, bins='log')
+        plt.xlabel("Original distances")
+        plt.ylabel("Projected distances")
+        cb = plt.colorbar()
+        cb.set_label("log10(count)")
+        plt.grid()
+        plt.savefig("streamhash_projection_" + metric + "_k"
+                    + str(k) + ".pdf", bbox_inches="tight")
